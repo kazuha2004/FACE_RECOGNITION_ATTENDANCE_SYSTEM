7d3d964729ce907a58e54c2c6cb4b8409d527750
sahilshukla302003 <sahilshukla302003@gmail.com>
2024-09-24 20:05:10 +0530

    as
    as

--- face_recognition.py
+++ face_recognition.py
@@ -19,16 +19,16 @@ class Face_Recognition:
         title_lbl = Label(self.root, text="FACE RECOGNITION", font=("Helvetica", 32, "bold"), bg="black", fg="white")
         title_lbl.place(x=0, y=0, width=1530, height=45)
 
-        # 1st image
-        img_top = Image.open(r"D:\wallpapers\facerecognizer1.jpg")
+        # 1st image(LEFT SIDE)
+        img_top = Image.open(r"D:\wallpapers\GOKU.jpg")
         img_top = img_top.resize((750, 900), Image.BILINEAR)
         self.photoimg_top = ImageTk.PhotoImage(img_top)
 
         f_lbl1 = Label(self.root, image=self.photoimg_top)
         f_lbl1.place(x=0, y=50, width=750, height=750)
 
-        # 2nd image
-        img_bottom = Image.open(r"D:\wallpapers\facerecognizer2.jpg")
+        # 2nd image(RIGHT SIDE)
+        img_bottom = Image.open(r"D:\wallpapers\Madara.jpg")
         img_bottom = img_bottom.resize((750, 900), Image.BILINEAR)
         self.photoimg_bottom = ImageTk.PhotoImage(img_bottom)
 
@@ -48,27 +48,28 @@ class Face_Recognition:
             coord = []
 
             for (x, y, w, h) in features:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                cv2.rectangle(img,(x, y), (x + w, y + h), (0, 255, 0), 3)
                 id, predict = clf.predict(gray_image[y:y + h, x:x + w])
                 confidence = int((100 * (1 - predict / 300)))
 
-                conn = mysql.connector.connect(host="localhost", username="root", password="0607", database="face_recognition")
+                conn = mysql.connector.connect(host="localhost", username="root", password="Sahil30@", database="face_recognition")
                 my_cursor = conn.cursor()
 
                 # Fetch Name
-                my_cursor.execute("SELECT Name FROM student WHERE Student_id = %s", (str(id),))
+                my_cursor.execute("SELECT Name FROM student WHERE Student_id="+str(id))
                 n = my_cursor.fetchone()
-                n = n[0] if n else "Unknown"  # If None, use "Unknown"
+                n =str(n)  # If None, use "Unknown"
+
 
                 # Fetch Roll
-                my_cursor.execute("SELECT Roll FROM student WHERE Student_id = %s", (str(id),))
+                my_cursor.execute("SELECT Roll FROM student WHERE Student_id="+str(id))
                 r = my_cursor.fetchone()
-                r = r[0] if r else "Unknown"  # If None, use "Unknown"
+                r =str(r)  # If None, use "Unknown"
 
                 # Fetch Department
-                my_cursor.execute("SELECT Dep FROM student WHERE Student_id = %s", (str(id),))
+                my_cursor.execute("SELECT Dep FROM student WHERE Student_id="+ str(id))
                 d = my_cursor.fetchone()
-                d = d[0] if d else "Unknown"  # If None, use "Unknown"
+                d =str(d)  # If None, use "Unknown"
 
                 if confidence > 77:
                     cv2.putText(img, f"Roll:{r}", (x, y - 55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
@@ -77,7 +78,6 @@ class Face_Recognition:
                 else:
                     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
                     cv2.putText(img, "Unknown face", (x, y - 55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
-
                 coord = [x, y, w, h]
 
             return coord
--- train.py
+++ train.py
@@ -1,14 +1,12 @@
+
 from tkinter import*
 from tkinter import ttk
 from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
-import cv2
 import os
 import numpy as np
-#pip install numpy
-#pip install opencv-contrib-python --user
-#pip install opencv-contrib-python
+import cv2
 
 class Train:
       def __init__(self,root):
@@ -19,60 +17,54 @@ class Train:
             title_lbl = Label(self.root, text=" TRAIN DATA SET", font=("Helvetica", 32, "bold"), bg="black",fg="white")
             title_lbl.place(x=0, y=0, width=1530, height=45)
 
-            # Load and resize the top image
-            img_top = Image.open(r"D:\wallpapers\train2.jpg")
+            img_top = Image.open(r"D:\wallpapers\attendance1.jpg")
             img_top = img_top.resize((1530, 325), Image.BILINEAR)
             self.photoimg_top = ImageTk.PhotoImage(img_top)
 
-            f_lbl1 = Label(self.root, image=self.photoimg_top)
-            f_lbl1.place(x=0, y=45, width=1530, height=325)
+            f_lbl2 = Label(self.root, image=self.photoimg_top)
+            f_lbl2.place(x=0, y=45, width=1530, height=325)
 
             #==================BUTTON=======================
-            b3_1 = Button(self.root, text="Train data",command=self.train_classifier, cursor="hand2", font=("Helvetica", 15, "bold"), bg="black",fg="white")
-            b3_1.place(x=0, y=380, width=1530, height=60)
 
-            # Load and resize the bottom image
-            img_bottom = Image.open(r"D:\wallpapers\train1.jpg")
-            img_bottom = img_bottom.resize((1530, 325), Image.BILINEAR)
+            b3_1 = Button(self.root, text="TRAIN DATA", command=self.train_classifier,cursor="hand2", font=("Helvetica", 30, "bold"), bg="black",fg="white")
+            b3_1.place(x=0, y=380, width=1530, height=60)#BUTTON UPER SIDE SHIFT KARNA PADEGA THODA YA PHIR UPAR VALI IMAGE KO NICHE KARNA PADEGA THODA SO PLEASE REMEMBER TO MAKE THE CHANGES IN FUTURE..........
+
+
+
+
+            img_bottom = Image.open(r"D:\wallpapers\student3.jpg")
+            img_bottom = img_top.resize((1530, 325), Image.BILINEAR)
             self.photoimg_bottom = ImageTk.PhotoImage(img_bottom)
 
-            f_lbl3 = Label(self.root, image=self.photoimg_bottom)
-            f_lbl3.place(x=0, y=450, width=1530, height=325)
+            f_lbl2 = Label(self.root, image=self.photoimg_bottom)
+            f_lbl2.place(x=0, y=440, width=1530, height=325)
 
+          #======================LBPH IS PRESENT HERE===================
       def train_classifier(self):
-            data_dir=("data")
+            data_dir="data"
             path=[os.path.join(data_dir,file) for file in os.listdir(data_dir)]
-            
-
-            faces=[] #image od the same person have the same id 
+            faces=[]
             ids=[]
-
-
             for image in path:
-                  img = Image.open(image).convert('L')  # gray scale image
-                  imageNp = np.array(img, 'uint8')
-                  # id = int(os.path.split(image)[1].split('.')[1])
-                  id = int(os.path.split(image)[1].split('.')[1])  
-
-
+                  img=Image.open(image).convert('L')# CONVERTING TO GRAY SCALE IMAGE
+                  imageNp=np.array(img,'uint8')
+                  id = int(os.path.split(image)[1].split('.')[1])
                   faces.append(imageNp)
                   ids.append(id)
-                  cv2.imshow("Training", imageNp)
-                  cv2.waitKey(1) == 13
-  
-            ids = np.array(ids)
+                  cv2.imshow("TRAINING",imageNp)
+                  cv2.waitKey(1)==13
+            ids=np.array(ids)
 
 
-#================================= Train the classifier and save =================================
-
-            
-            clf = cv2.face.LBPHFaceRecognizer_create()
+          #======================TRAIN THE CLASSIFIER AND SAVE=============================
+            clf=cv2.face.LBPHFaceRecognizer_create()#pip install opencv-contrib-python   WARNA ERROR DEGA
             clf.train(faces,ids)
-            clf.write('classifier.xml')
+            clf.write("classifier.xml")
             cv2.destroyAllWindows()
-            messagebox.showinfo('Result',"Training data sets completed!!",parent=self.root)
+            messagebox.showinfo("RESULT","TRAINING DATASET COMPLETED")
 
-            
+
+
 
 if __name__ == "__main__":
       root=Tk()
